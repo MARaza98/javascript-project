44893fa1c2988911e3933b0f7aa743ecc15e858b
Muhammad Ahmed Raza <68612806+MARaza98@users.noreply.github.com>
2022-05-19 00:10:35 +0500

    done task

---
+++ javascript.js
@@ -0,0 +1,149 @@
+// // chapter3
+// // answer 1
+// var age = 23
+// alert("i am "+age+" year old")
+
+// // answer 2
+// var visit = 14
+// alert("you have visited this site "+visit+"times")
+
+// // answer 3
+// var birth = 1998
+// document.write("My birth year is "+ birth + "<br>")
+// document.write("Data type of my declared variable is number")
+
+// // answer 4
+// var name = prompt("Enter your name")
+// var product = prompt("Enter your product")
+// document.write(name +" ordered "+ product +" on XYZ Clothing store")
+
+// chapter 4
+// // answer 1
+// var name , age , city
+
+// // answer 2
+// // legal variable
+// var var69_a
+// var snake_1
+// var $price
+// var firstname
+// var _my_name
+
+// // illegal variable
+// var 69var_a
+// var $nake#1
+// var @gmail
+// var 2name
+// var birth-year
+
+// // answer 3
+// document.write("<b>Rules for naming JS variables</b><br><br><br>")
+// document.write("Variable names can only contain number,$ and. For example: $my_1stVariable<br>")
+// document.write("variable must begin with a letter $ or. For example: $name,name or name<br>")
+// document.write("variable names are case sensitive<br>")
+// document.write("variable names should not be js keywords")
+ 
+// chapter 5 
+// // answer 1
+// var num1 = 3
+// var num2 = 5
+// var result = num1 + num2
+// document.write("sum of 3 and 5 is " + result + "<br>")
+
+// // answer 2
+// var num1 = 3
+// var num2 = 5
+// var result = num1 - num2
+// document.write("subtraction of 3 and 5 is " + result + "<br>")
+
+// var num1 = 3
+// var num2 = 5
+// var result = num1 * num2
+// document.write("multiplication of 3 and 5 is " + result + "<br>")
+
+// var num1 = 3
+// var num2 = 5
+// var result = num1 / num2
+// document.write("division of 3 and 5 is " + result + "<br>")
+
+// var num1 = 9
+// var num2 = 9
+// var result = num1 % num2
+// document.write("modulus of 9 and 9 is " + result + "<br>")
+
+// //  answer 3
+// var number1;
+//     document.write("Value after variable declaration is:" + number1 + "<br>");
+
+//     number1 = 5;
+//     document.write("Value after variable declaration is:" + number1 + "<br>");
+
+//     number1++
+//     document.write("Initial value: " + number1 + "<br>");
+
+//     number1 = number1 + 7;
+//     document.write("Value after addition is:" + number1 + "<br>");
+
+//     number1--;
+//     document.write("Value after decrement is:" + number1 + "<br>");
+
+//     number1 = number1 % 3;
+//     document.write("The remainder is : " + number1 + "<br>");
+
+// // answer 4
+// var value = 600
+// var ticket = 5
+// var result = 600 * 5
+// document.write("Total cost to buy 5 tickets to a movie is " + result +"PKR")
+
+// // Question 5
+// var tableNumber = 4;
+// document.write("<h2>Table of 4 </h2>")
+// document.write("4x1= " + 4 * 1 + "<br>" + "4x2= " + 4 * 2 + "<br>" + "4x3= " + 4 * 3 + "<br>" + "4x4= " + 4 * 4 + "<br>" + "4x5= " + 4 * 5 + "<br>" + "4x6= " + 4 * 6 + "<br>" + "4x7= " + 4 * 7 + "<br>" + "4x8= " + 4 * 8 + "<br>" + "4x9= " + 4 * 9 + "<br>" + "4x10= " + 4 * 10 + "<br>")
+
+// // Question 6
+// var Celsius = 25;
+// var fahrenheit = 70;
+
+// c = (fahrenheit - 32) * 5 / 9;
+// f = (Celsius * 9 / 5) + 32;
+
+// document.write(Celsius + "<sup>o</sup>C is " + f + "<sup>o</sup>C");
+// document.write("<br>");
+// document.write(fahrenheit + "<sup>o</sup>F is " + c + "<sup>o</sup>C ");
+
+
+
+// // Question 7
+
+// var item1 = 4000;
+// var item2 = 2500;
+// var item1Qty = 5;
+// var item2Qty = 2;
+// var shippingCharges = 800;
+
+// var priceViaQtyItem1 = item1 * item1Qty;
+// var priceViaQtyItem2 = item2 * item2Qty;
+
+// var priceCal = priceViaQtyItem1 + priceViaQtyItem2;
+
+// var totalCost = priceViaQtyItem1 + priceViaQtyItem2 + shippingCharges;
+
+// document.write("<h2>Your Order</h2>");
+
+// document.write("PRODUCT ----------------------------- SUBTOTAL");
+// document.write("<br>");
+// document.write("AI Watch (item1)  × " + item1Qty + " ----------------- " + priceViaQtyItem1 + "PKR");
+// document.write("<br>");
+// document.write("Mac Book (item2)  × " + item2Qty + " ----------------- " + priceViaQtyItem2 + "PKR");
+// document.write("<br>");
+// document.write("Subtotal " + " ---------------------------------- " + priceCal + "PKR");
+// document.write("<br>");
+// document.write("Total cost of your order is :  " + " --------- " + totalCost + "PKR");
+
+// Question 8
+var totalMarks = 980;
+var obtMarks = 804;
+var per = obtMarks / totalMarks * 100;
+
+document.write("Total Marks : " + totalMarks + "<br>Obtained Marks : " + obtMarks + "<br>Percentage : " + per + "%");
