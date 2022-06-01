63fcf440817ed9635676e97f515bdac14561dddf
Muhammad Ahmed Raza <68612806+MARaza98@users.noreply.github.com>
2022-06-01 15:10:53 +0500

    assigment__done

---
+++ home pretic/array_pertic.js
@@ -0,0 +1,18 @@
+
+// var firstname = prompt("Enter your name ")
+// var input = firstname.split(" ")
+// console.log(input)
+
+// var str = ["my","name","is","ahmed","raza"]
+// var save = str.join(" ")
+// console.log(save)
+
+// var input = prompt("Enter your palatrum word")
+// var array = input.split("")
+// var rever = array.reverse()
+// var joined = rever.join(" ")
+// if (input === joined) {
+//     alert("this is correct answer")
+// }else{
+//     alert("this is wrong answer")
+// }
