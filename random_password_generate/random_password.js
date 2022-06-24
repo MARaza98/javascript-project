4fbd7dd2586243ed131892b22bedccc360c16b10
Muhammad Ahmed Raza <68612806+MARaza98@users.noreply.github.com>
2022-06-24 22:11:05 +0500

    done_assigment

---
+++ random_password_generate/random_password.js
@@ -0,0 +1,36 @@
+// // Password Generated 
+
+// var random = ["a", "A", "b", "c", "C", "d", "D", "E", 'e', "F", 'f', "g",
+// "G", 'H', 'h', 'i', 'I', 'J', 'j', "K", "k", "L", "l", "M",
+// "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s",
+// "T", "t", "U", 'u', "V", 'v', "W", 'w', "x", "X", 'y', "Y", "Z", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (var i = 0; i <= 8; i++) {
+
+//     var randomNum = Math.floor(Math.random() * random.length);
+//     // document.write("<h1><br>Genreat Random Password :</h1>"+random[randomNum]);
+//     document.write(random[randomNum]);
+// }
+
+// // Head _ Till _ Game
+
+// var userName = prompt("enter name :");
+// var userInput = prompt("Enter H/T:").toUpperCase();
+// var arr = ["H", "T"]
+// var randomNum = arr[Math.floor(Math.random() * arr.length)]
+// // (arr[randomNum]);
+
+// if (userInput === randomNum) {
+//     alert(userName + " win");
+// } else {
+//     alert(userName + " lost");
+
+// }
+
+// // Dice _ Game
+
+// var bigDecimal = Math.random();
+// var improvedNum = (bigDecimal * 6) + 1;
+// var numberOfStars = Math.floor(improvedNum);
+
+// document.write("<h2> dice number :" + numberOfStars + "</h2><br>")
